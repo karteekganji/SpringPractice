9186d39f100b902ea56b27818f88ba8115a9990d
Karteek Ganji <karteek.ganji@thrymr.net>
2018-07-30 02:43:45 +0530

    Swagger integration

---
+++ src/main/java/com/spring/utils/Swagger.java
@@ -0,0 +1,34 @@
+package com.spring.utils;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@Configuration
+@EnableSwagger2
+public class Swagger {
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.basePackage("com.spring.controller"))
+                .paths(regex("/*.*"))
+                .build()
+                .apiInfo(apiInfo());
+
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Practice Springboot API Documentation")
+                .description("Here you can find all the APIs")
+                .build();
+    }
+}
+
